41aaa174b5d224774398e9cb8946ad6dec4602e2
Kim, Sojin <xozinee@gmail.com>
2023-10-18 10:55:06 +0900

    two

--- src/g202114150/Main.java
+++ src/g202114150/Main.java
@@ -3,6 +3,7 @@ package g202114150;
 public class Main {
 
   public static void main(String[] args) {
+    Two.print();
     System.out.println("one 202114150");
   }
 }
---
+++ src/g202114150/Two.java
@@ -0,0 +1,8 @@
+package g202114150;
+
+public class Two {
+
+  public static void print() {
+    System.out.println("two");
+  }
+}
